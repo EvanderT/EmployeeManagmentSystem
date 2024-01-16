78baf86eafeb864b66d294ec47acdfcdad510180
EvanderTx <73314035+EvanderT@users.noreply.github.com>
2024-01-16 09:48:12 +0100

    update entidades

--- BaseLibrary/Entities/BaseEntity.cs
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace BaseLibrary.Entities
@@ -10,5 +11,10 @@ namespace BaseLibrary.Entities
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+
+
+        //RelationShip: One to Many
+        [JsonIgnore]
+        public List<Employee>? Employees { get; set; }
     }
 }
